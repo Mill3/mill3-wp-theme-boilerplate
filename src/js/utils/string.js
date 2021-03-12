d1cdfc494288f99105837752be6d4d5e074c0664
Antoine Girard <thetoine@gmail.com>
2021-03-12 10:21:20 -0500

    Barba-dom-controller plugin: refactor of the chunk loader but without using Webpack chunk loading. Added PascalCase converter in string utils.

--- src/js/utils/string.js
+++ src/js/utils/string.js
@@ -1,3 +1,9 @@
 String.prototype.capitalize = function () {
   return this.charAt(0).toUpperCase() + this.slice(1);
 };
+
+export const PascalCase = (s) => {
+  return (s||'').toLowerCase().replace(/(\b|-)\w/g, (m) => {
+    return m.toUpperCase().replace(/-/,'');
+  });
+}
