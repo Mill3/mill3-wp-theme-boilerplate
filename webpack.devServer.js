949d04d927b71664fac25d7091173c53352f55d9
Antoine Girard <thetoine@gmail.com>
2021-08-20 15:30:32 -0400

    webpack-dev-server 4.x with updated config

--- webpack.devServer.js
+++ webpack.devServer.js
@@ -7,13 +7,21 @@ export const webpackDevServer = (DEV = false) => {
           devServer: {
             port: SERVER_PORT,
             hot: true,
-            contentBase: PATHS.dist,
-            publicPath: `http://localhost:${SERVER_PORT}/`,
+            compress: true,
+            devMiddleware: {
+              publicPath: `http://localhost:${SERVER_PORT}/`,
+            },
+            static: {
+              publicPath: PATHS.dist,
+            },
+            client: {
+              logging: "info",
+              overlay: true,
+              progress: true,
+            },
             headers: {
               "Access-Control-Allow-Origin": `*`
-            },
-            transportMode: 'ws',
-            stats: 'minimal'
+            }
           }
         }
       : {})
