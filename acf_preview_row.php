ffa852cde17dfa9fb4cd7646b49a671905b4ba40
Dominic Mercier <d.mercier84@gmail.com>
2021-11-03 14:53:18 -0400

    acf preview :: add post-type to body classname

--- acf_preview_row.php
+++ acf_preview_row.php
@@ -1,10 +1,14 @@
 <?php
 
+global $post;
+$post_type = $post->post_type;
+
 $context = Timber\Timber::get_context();
 $context['post'] = get_row(true);
 $context['is_preview'] = $is_preview;
 $context['stylesheet'] = Mill3WP\Assets\Asset_File_path('acfPreview', 'css');
 $context['js'] = Mill3WP\Assets\Asset_File_path('acfPreviewIframe', 'js');
+$context['body_class'] = 'single-' . $post_type;
 
 $doc = Timber\Timber::compile("base-acf-preview.twig", $context);
 
