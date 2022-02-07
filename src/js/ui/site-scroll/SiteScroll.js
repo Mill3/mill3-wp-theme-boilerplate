1a5e1e2a8e1f42ef69b6ec1fc072ec7026a3e9a3
Dominic Mercier <d.mercier84@gmail.com>
2022-02-07 15:37:42 -0500

    site-scroll :: calculate pb-row-wrapper height when using smooth scroll

--- src/js/ui/site-scroll/SiteScroll.js
+++ src/js/ui/site-scroll/SiteScroll.js
@@ -1,12 +1,15 @@
 import LocomotiveScroll from "locomotive-scroll";
 
-import { $, body, html } from "@utils/dom";
+import { $, $$, body, html, rect } from "@utils/dom";
+import { mobile } from "@utils/mobile";
+import ResizeOrientation from "@utils/resize";
 
 export const SELECTOR = "[data-scroll-container]";
 export const SCROLL_MIN_CLASSNAME = "--js-scroll-min";
 export const SCROLL_DOWN_CLASSNAME = "--js-scroll-down";
 export const SCROLL_UP_CLASSNAME = "--js-scroll-up";
 export const SCROLL_DISABLE_CLASSNAME = "--js-scroll-disabled";
+export const SCROLL_SECTION_SELECTOR = ".pb-row-wrapper[data-scroll-section]";
 
 const SCROLL_THRESHOLD = 200;
 const SCROLL_DIFF_THRESHOLD = 2;
@@ -25,6 +28,7 @@ class SiteScroll {
 
     this._onCall = this._onCall.bind(this);
     this._onScroll = this._onScroll.bind(this);
+    this._onResize = this._onResize.bind(this);
 
     this.start = this.start.bind(this);
     this.stop = this.stop.bind(this);
@@ -49,6 +53,15 @@ class SiteScroll {
     this.scrollDirection = null;
     this.previousScrollY = 0;
     this.el = $(SELECTOR);
+    this.scrollSections = [ ...$$(SCROLL_SECTION_SELECTOR, this.el) ].map(section => {
+      return {
+        section: section,
+        target: section.firstElementChild,
+      }
+    });
+
+    if( !this.ro ) this.ro = new ResizeOrientation(this._onResize);
+    this.ro.run();
 
     this.scroll = new LocomotiveScroll({
       el: this.el,
@@ -66,6 +79,7 @@ class SiteScroll {
   }
 
   destroy() {
+    if (this.ro) this.ro.off();
     if (this.scroll) {
       this.scroll.off("call", this._onCall);
       this.scroll.off("scroll", this._onScroll);
@@ -81,6 +95,7 @@ class SiteScroll {
     }
 
     this.el = null;
+    this.scrollSections = null;
     this.scroll = null;
     this.initialized = false;
     this.hasScrolledAboveThreshold = false;
@@ -89,6 +104,7 @@ class SiteScroll {
   }
 
   update() {
+    if (this.ro) this.ro.run();
     if (this.scroll) this.scroll.update();
   }
 
@@ -113,6 +129,9 @@ class SiteScroll {
   }
 
   _bindEvents() {
+    // start listening for window's resize event
+    if( this.ro ) this.ro.on();
+
     // register event on global emmiter system
     if (!this.emitter) return;
 
@@ -181,6 +200,17 @@ class SiteScroll {
       // emit scroll direction change to application
       this.emitter.emit(`${this.name}.direction`, direction);
     }
+  }
+
+  _onResize() {
+    // do nothing for mobile
+    if( mobile ) return;
+
+    // force height for each sections to avoid overscrolling OR cropping at bottom of the page when using smooth-scroll
+    this.scrollSections.forEach(({section, target}) => {
+      const { height } = rect(target);
+      section.style.setProperty('--row-height', `${Math.floor(height)}px`);
+    });
   }
 
   // getter - setter
