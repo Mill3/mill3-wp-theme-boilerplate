68e94ac24135b44293b45efd382dae98249f1dfe
Antoine Girard <thetoine@gmail.com>
2022-02-18 12:14:16 -0500

    bumped Axios version, postcss-preset-env bump and proper config

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,11 @@ module.exports = {
   syntax: 'postcss-scss',
   plugins: {
     "postcss-preset-env": {
-      browsers: "last 2 versions"
+      autoprefixer: {
+        grid: false,
+        flex: false,
+        supports: true
+      }
     }
   }
 };
