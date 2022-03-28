e605f04d8d2f59851ffc7cebff110db6071003db
Dominic Mercier <d.mercier84@gmail.com>
2022-03-28 13:40:03 -0400

    site-header invert option for Pages (buggy on Chrome)

--- lib/filters.php
+++ lib/filters.php
@@ -7,12 +7,19 @@
 function theme_body_class($classes) {
     global $post;
 
-    // invert site-header's color for home
-    if ( is_front_page() ) {
-        $classes[] = 'site-header-inverted';
-    }
+    // by default, site-header isn't inverted
+    $site_header_inverted = FALSE;
+
+    // get site_header_inverted bool from ACF values in global $post
+    if( is_singular(['page']) ) $site_header_inverted = get_field('site_header_inverted', $post);
+
+    // invert site-header's color for homepage (activate if required for your project)
+    //if ( is_front_page() ) $site_header_inverted = TRUE;
+
+    // add classname to body if site_header_inverted is TRUE
+    if ( $site_header_inverted ) $classes[] = '--site-header-inverted';
 
     return $classes;
 }
 
-//add_filter( 'body_class', 'theme_body_class', 11, 2);
+add_filter( 'body_class', 'theme_body_class', 11, 2);
