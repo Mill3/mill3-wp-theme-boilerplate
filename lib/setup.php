ae278b6ae33c60a0ca4b0edcad9aaaf8a77988b9
Dominic Mercier <d.mercier84@gmail.com>
2022-03-28 11:09:54 -0400

    admin :: set Pages menu first

--- lib/setup.php
+++ lib/setup.php
@@ -126,6 +126,41 @@ add_filter(
 );
 
 
+/**
+ * Filters WordPress' default menu order
+ */
+function mill3wp_admin_menu_order( $menu_order ) {
+    // define your new desired menu positions here
+    // for example, move 'upload.php' to position #9 and built-in pages to position #1
+    $new_positions = array(
+      //'upload.php' => 9,
+      'edit.php?post_type=page' => 1
+    );
+    // helper function to move an element inside an array
+    function move_element(&$array, $a, $b) {
+      $out = array_splice($array, $a, 1);
+      array_splice($array, $b, 0, $out);
+    }
+
+    // traverse through the new positions and move 
+    // the items if found in the original menu_positions
+    foreach( $new_positions as $value => $new_index ) {
+      if( $current_index = array_search( $value, $menu_order ) ) {
+        move_element($menu_order, $current_index, $new_index);
+      }
+    }
+
+    return $menu_order;
+  };
+
+
+// Activate 'menu_order' filter
+add_filter('custom_menu_order', function() { return true; });
+
+// Change admin menu order
+add_filter('menu_order', __NAMESPACE__ . '\\mill3wp_admin_menu_order');
+
+
 // Set directory for Admin Columns settings
 add_filter('acp/storage/file/directory', function() {
   return get_stylesheet_directory() . '/acp-settings';
