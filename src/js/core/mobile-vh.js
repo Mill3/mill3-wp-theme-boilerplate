efc9ef5344cddfcaa4ccce07fe5834819eca0d23
Dominic Mercier <d.mercier84@gmail.com>
2022-03-30 15:11:01 -0400

    lvh & svh fallback

--- src/js/core/mobile-vh.js
+++ src/js/core/mobile-vh.js
@@ -10,20 +10,42 @@ import Viewport from "../utils/viewport";
  * Init in main application and add the following to a selector
  *
  * .selector {
- *    height: calc(var(--vh) * 100);
+ *    height: calc(var(--vh) * 100); // dynamic viewport height
+ *    height: calc(var(--lvh) * 100); // large viewport height (without UA Interfaces)
+ *    height: calc(var(--svh) * 100); // small viewport height (with UA Interfaces)
  * }
  *
  */
 
 const MobileViewportUnit = (() => {
+  let lvh = 0;
+  let svh = 0;
+  let vh = 0;
+
   const onResize = () => {
-    html.style.setProperty("--vh", `${Viewport.height * 0.01}px`);
+    vh = Viewport.height * 0.01;
+    
+    // set svh (short viewport height)
+    if( !svh ) svh = vh;
+
+    // update lvh (long viewport height) if smaller than viewport height
+    if( lvh < vh ) lvh = vh;
+
+    html.style.setProperty("--vh", `${vh}px`);
+    html.style.setProperty("--lvh", `${lvh}px`);
+    html.style.setProperty("--svh", `${svh}px`);
+  };
+  const onOrientationChange = () => {
+    lvh = 0;
+    svh = 0;
   };
 
   const ctx = {
     init: () => {
       onResize();
-      on(window, "orientationchange", onResize);
+
+      on(window, "orientationchange", onOrientationChange);
+      on(window, "resize", onResize);
     }
   };
 
