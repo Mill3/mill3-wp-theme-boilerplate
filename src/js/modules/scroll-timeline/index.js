a0a9f7180305adab64da3cd706a69192da49eb41
Dominic Mercier <d.mercier84@gmail.com>
2022-03-30 16:45:33 -0400

    scroll-timeline  - ability to disable scroll-timeline for mobile  - ability to define a different scroll-timeline for mobile

--- src/js/modules/scroll-timeline/index.js
+++ src/js/modules/scroll-timeline/index.js
@@ -2,12 +2,11 @@
  * SCROLL TIMELINE
  *
  * How to use:
- * Add data-module="scroll-timeline" to your HTML element
- * Add data-timeline="your-animejs-animation-object" (must be JSON encode & html escaped)
- *
- * Optionally add data-scroll & data-scroll-call="timeline" to your HTML element.
- * If you omit data-scroll or data-scroll-call, they will be inserted automatically.
- * If you provide them, it will be faster for mobile device.
+ * Add [data-scroll] attribute to your HTML element
+ * Add [data-scroll-call="timeline"] attribute to your HTML element
+ * Add [data-module="scroll-timeline"] attribute to your HTML element
+ * Add [data-timeline="your-animejs-animation-object"] (must be JSON encode & html escaped)
+ * Add [data-timeline-mobile="your-animejs-animation-object"] (must be JSON encode & html escaped)
  *
  * Example:
  * <div
@@ -22,16 +21,21 @@
  *
  * The whole data-timeline JSON object is passed to your AnimeJS animation.
  * See https://animejs.com/documentation/ for more details on passing data to AnimeJS.
+ * 
+ * 
+ * Different scroll animation for mobile device (viewport < 768px).
+ * Add [data-timeline-mobile="your-animejs-animation-object"] (must be JSON encode & html escaped)
+ * 
+ * Disable scroll animation for mobile device (viewport < 768px).
+ * Add [data-timeline-mobile] attribute, without value, to your HTML element.
  */
 
 import anime from "animejs";
 
-import { $$ } from "@utils/dom";
 import Viewport from "@utils/viewport";
 
 import Module from "../module/Module";
 
-const SELECTOR = "[data-timeline]";
 const TIMELINE_DEFAULTS = {
   easing: "linear",
   autoplay: false,
@@ -57,30 +61,17 @@ class ScrollTimeline extends Module {
 
   init() {
     this.initialized = true;
-    this.items = [];
-
-    [...$$(SELECTOR)].forEach((el, index) => {
-      if (el.hasAttribute("data-scroll-call") && el.dataset.scrollCall !== "timeline") {
-        console.warn(
-          `ScrollTimeline will be disabled on ${el} because it already has a data-scroll-call attribute.`,
-          el
-        );
-        return;
-      }
-
-      if (!el.hasAttribute("data-scroll")) el.setAttribute("data-scroll", "");
-      if (!el.hasAttribute("data-scroll-scroll") || el.dataset.scrollCall !== "timeline")
-        el.setAttribute("data-scroll-call", "timeline");
-
-      el.dataset.timelineID = index;
-    });
+    this.items = new Map();
 
     this._bindEvents();
   }
   destroy() {
     this._unbindEvents();
 
-    if (this.items) this.items.forEach(tl => anime.remove(tl.el));
+    if (this.items) {
+      this.items.forEach(tl => anime.remove(tl.el));
+      this.items.clear();
+    }
 
     this.initialized = false;
     this.items = null;
@@ -95,48 +86,64 @@ class ScrollTimeline extends Module {
     this.emitter.off("SiteScroll.scroll", this._onScroll);
   }
 
-  _timelineExists(id) {
-    return this._getTimeline(id) !== undefined;
-  }
-  _getTimeline(id) {
-    return this.items.find(tl => tl.id === id);
-  }
-
   _onCall(direction, obj) {
+    // do nothing for "exit" events
     if (direction !== "enter") return;
 
     const { el } = obj;
-    const id = el.dataset.timelineID || 0;
 
-    // if this object timeline already exists,
-    // update object top/bottom in timeline and stop here
-    if (this._timelineExists(id)) {
-      const tl = this._getTimeline(id);
-      tl.top = obj.top;
-      tl.bottom = obj.bottom;
+    // set timeline ID if not exists
+    if( !el.hasAttribute('data-timeline-id') ) el.setAttribute('data-timeline-id', this.items.size);
+
+    // read timeline ID
+    const id = el.dataset.timelineId;
+
+    // if this timeline already exists, update top/bottom in timeline and stop here
+    if ( this.items.has(id) ) {
+      const tl = this.items.get(id);
+            tl.top = obj.top;
+            tl.bottom = obj.bottom;
 
       return;
     }
 
-    const timelineArgs = JSON.parse(el.dataset.timeline);
+    let timelineArgs;
+
+    if( Viewport.width < 768 ) {
+      // if element has no [data-timeline-mobile], fallback to [data-timeline]
+      if( !el.hasAttribute('data-timeline-mobile') ) timelineArgs = JSON.parse(el.dataset.timeline);
+      // if [data-timeline-mobile] is empty, stop here
+      else if( !el.dataset.timelineMobile ) return;
+      // JSON parse [data-timeline-mobile]
+      else timelineArgs = JSON.parse(el.dataset.timelineMobile);
+    } 
+    else {
+      // JSON parse [data-timeline]
+      timelineArgs = JSON.parse(el.dataset.timeline);
+    }
+    
+    // create AnimeJS timeline from best matching [data-timeline] or [data-timeline-mobile]
+    const timeline = anime({ targets: el, ...TIMELINE_DEFAULTS, ...timelineArgs });
 
     // create timeline and store it
-    this.items.push({
-      id: id,
+    this.items.set(id, {
       top: obj.top,
       bottom: obj.bottom,
+      ref: obj,
       el: el,
-      timeline: anime({
-        targets: el,
-        ...TIMELINE_DEFAULTS,
-        ...timelineArgs
-      })
+      timeline: timeline,
     });
   }
   _onScroll(y) {
+    // abort if there is no registered animations
+    if( this.items.size === 0 ) return;
+
     const vh = Viewport.height;
 
     this.items.forEach(tl => {
+      // skip if element is not in view
+      if( !tl.ref.inView ) return;
+
       const { timeline, top, bottom } = tl;
 
       const limit = Math.min(bottom, vh + bottom - top);
