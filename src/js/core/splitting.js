1f29971f8bd046ca4b5ea184628ec5313c526ab0
Dominic Mercier <d.mercier84@gmail.com>
2022-05-04 11:43:45 -0400

    splitting :: new default splitting method for better performance

--- src/js/core/splitting.js
+++ src/js/core/splitting.js
@@ -3,9 +3,25 @@ import Splitting from "splitting";
 import { $$ } from "@utils/dom";
 
 
+const splitByWordsForMaskAnimation = (el, options, ctx) => {
+  const { words } = ctx;
+  words.forEach(word => word.innerHTML = `<span class="wordText">${word.innerHTML}</span>`);
+
+  return [];
+};
+
+// split by words and wrap each word content into another span to perform masking animation
+Splitting.add({
+  by: 'wordsMask',
+  key: 'wordsMask',
+  depends: ['words'],
+  split: splitByWordsForMaskAnimation,
+});
+
+
 export default () => {
   [ ...$$('[data-splitting]') ].forEach(el => {
-    const splittingMethod = el.dataset.splitting || "chars";
+    const splittingMethod = el.dataset.splitting || "wordsMask";
 
     Splitting({
       target: el,
