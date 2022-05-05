a37356d0789646968415ee78bf0d89fe1385e3f1
Antoine Girard <thetoine@gmail.com>
2022-05-05 12:40:25 -0400

    cli for js MODULES and UI

--- _templates/cli.js
+++ _templates/cli.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
+const chalk = require("chalk");
 const path = require("path");
 const fs = require("fs-extra");
 const ejs = require("ejs");
@@ -8,7 +9,7 @@ const argv = require("yargs-parser")(process.argv.slice(2));
  * MILL3 - Theme CLI utils for JS and CSS modules
  *
  * argument source : [js/css]
- * argument type : [module/ui]
+ * argument type : [modules/ui]
  * options : --name MyFooBarModule
  *
  * Examples :
@@ -20,7 +21,7 @@ const argv = require("yargs-parser")(process.argv.slice(2));
  */
 
 const ALLOWED_SOURCES = ['js', 'scss'];
-const ALLOWED_TYPES = ['module', 'ui'];
+const ALLOWED_TYPES = ['modules', 'ui'];
 
 const src_path = path.resolve(__dirname, '../src');
 console.log('src_path:', src_path)
@@ -46,7 +47,17 @@ const cli = () => {
     // in JS source, we store each module in its own directory : ../src/js/[module/ui]/[module_slug]/MySuperModule.js
     if (source == 'js') {
       source_destination = path.join(source_destination, type, module_slug);
-      createJSModule(source_destination, type, name, module_slug);
+      console.log('source_destination:', source_destination)
+      switch (type) {
+        case 'modules':
+          createJSModule(source_destination, type, name, module_slug)
+          break;
+        case 'ui':
+          createJSUi(source_destination, type, name, module_slug)
+          break;
+        default:
+
+      }
       process.exit(1);
     }
 
@@ -56,30 +67,12 @@ const cli = () => {
     }
 
 
-    // const filename = path.join(__dirname, type, component, `./${component}.ejs`)
-    // console.log('filename:', filename)
-
-    // const data = {
-    //   fn: name,
-    //   arguments: commands
-    // }
-
-    // ejs.renderFile(filename, data, {}, function(err, str) {
-    //   // str => Rendered HTML string
-    //   if (err) {
-    //     console.error(err)
-    //   }
-
-    //   console.log(str)
-    // })
-
   } catch (err) {
     console.error(err)
   }
 }
 
 const createJSModule = (destination, type, name, module_slug) => {
-  // console.log('destination, name, slug:', type, destination, name)
   const template_index = path.join(__dirname, 'sources/js', type, `index.ejs`)
   const template_factory = path.join(__dirname, 'sources/js', type, `factory.ejs`)
   const template_module = path.join(__dirname, 'sources/js', type, `ModuleTemplate.ejs`)
@@ -94,8 +87,10 @@ const createJSModule = (destination, type, name, module_slug) => {
     if (err) {
       console.error(err)
     }
-
-    console.log(str)
+    const outputFile = path.join(destination, `${name}.js`)
+    fs.ensureFileSync(outputFile)
+    fs.outputFileSync(outputFile, str)
+    console.log(`File ${outputFile} created!`);
   })
 
   ejs.renderFile(template_factory, data, {}, function(err, str) {
@@ -103,9 +98,55 @@ const createJSModule = (destination, type, name, module_slug) => {
     if (err) {
       console.error(err)
     }
-
-    console.log(str)
+    const outputFile = path.join(destination, `factory.js`)
+    fs.ensureFileSync(outputFile)
+    fs.outputFileSync(outputFile, str)
+    console.log(`File ${outputFile} created!`);
   })
+
+  ejs.renderFile(template_index, data, {}, function(err, str) {
+    // str => Rendered HTML string
+    if (err) {
+      console.error(err)
+    }
+    const outputFile = path.join(destination, `index.js`)
+    fs.ensureFileSync(outputFile)
+    fs.outputFileSync(outputFile, str)
+    console.log(`File ${outputFile} created!`);
+  })
+}
+
+const createJSUi = (destination, type, name, module_slug) => {
+  const template_index = path.join(__dirname, 'sources/js', type, `index.ejs`)
+  const template_ui = path.join(__dirname, 'sources/js', type, `UiTemplate.ejs`)
+
+  const data = {
+    ModuleName: name,
+    module_slug: module_slug
+  }
+
+  ejs.renderFile(template_index, data, {}, function(err, str) {
+    // str => Rendered HTML string
+    if (err) {
+      console.error(err)
+    }
+    const outputFile = path.join(destination, `index.js`)
+    fs.ensureFileSync(outputFile)
+    fs.outputFileSync(outputFile, str)
+    console.log(`File ${outputFile} created!`);
+  })
+
+  ejs.renderFile(template_ui, data, {}, function(err, str) {
+    // str => Rendered HTML string
+    if (err) {
+      console.error(err)
+    }
+    const outputFile = path.join(destination, `${name}.js`)
+    fs.ensureFileSync(outputFile)
+    fs.outputFileSync(outputFile, str)
+    console.log(`File ${outputFile} created!`);
+  })
+
 }
 
 /**
@@ -127,19 +168,19 @@ const validateArgv = (source, type, name) => {
 
   // 3. check if has source & type
   if (!source || !type) {
-    console.error("`source` and `type` arguments are required : cli [js/scss] [module/ui]")
+    console.error("`source` and `type` arguments are required : cli [js/scss] [modules/ui]")
     process.exit(1)
   }
 
   // 4. check if source type is allowed
   if(!ALLOWED_TYPES.some((s) => s === type)) {
-    console.error("`type` argument is invalid : cli [module/ui]")
+    console.error("`type` argument is invalid : cli [modules/ui]")
     process.exit(1)
   }
 
   // 5. check if --name option is defined
   if(!name) {
-    console.error("`name` argument is required : cli [js/scss] [module/ui] MyFooBarModule")
+    console.error("`name` argument is required : cli [js/scss] [modules/ui] MyFooBarModule")
     console.error("Important : you must name your module in `PascalCase` style.")
     process.exit(1)
   }
