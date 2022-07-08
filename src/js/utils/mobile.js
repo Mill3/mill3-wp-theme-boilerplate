c593c28897375829eaa2bd676c8a52033e5ec94c
Dominic Mercier <d.mercier84@gmail.com>
2022-07-08 10:50:31 -0400

    detect touch device in JS

--- src/js/utils/mobile.js
+++ src/js/utils/mobile.js
@@ -3,6 +3,10 @@ export const mobile =
   /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ||
   (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
 
+
+export const touch_device = window.matchMedia('(hover: none)').matches;
+
 export default {
-  mobile
+  mobile,
+  touch_device
 };
