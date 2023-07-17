e12838a9d58b4b5e77503ab98e28c754b7272612
Dominic Mercier <d.mercier84@gmail.com>
2023-07-17 13:30:25 -0400

    math utils :: image's contain algorythm

--- src/js/utils/math.js
+++ src/js/utils/math.js
@@ -15,6 +15,23 @@ export const cover = (width, height, ratio) => {
   };
 };
 
+export const contain = (width, height, ratio) => {
+  let w = width;
+  let h = w / ratio;
+
+  if (h > height) {
+    h = height;
+    w = h * ratio;
+  }
+
+  return {
+    x: (width - w) * 0.5,
+    y: (height - h) * 0.5,
+    width: w,
+    height: h
+  };
+};
+
 /**
  * Calculate hypothenuse of a triangle
  */
@@ -56,6 +73,7 @@ export const limit = (min, max, value) => Math.max(min, Math.min(max, value));
 
 export default {
   cover,
+  contain,
   hypothenuse,
   pythagore,
   degreeToRad,
