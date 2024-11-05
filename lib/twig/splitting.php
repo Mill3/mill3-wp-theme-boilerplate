e3b40fb72a55b8d7e05455ab3d08107ab90db1e8
Dominic Mercier <d.mercier84@gmail.com>
2024-11-05 16:19:46 -0500

    splitting :: word regex

--- lib/twig/splitting.php
+++ lib/twig/splitting.php
@@ -4,9 +4,14 @@ namespace Mill3\Twig;
 
 class Splitting {
 
-    // Regex pour matcher chaque mot en dehors des balises HTML
-    // Regex pour capturer les mots avec apostrophes internes, trait d'union et ponctuation terminale, sans la perdre
-    private static $WORD_REGEX = '/(<[^>]+>)|(\b[\w\p{L}]+(?:[\'’\-][\w\p{L}]+)*[.,;:!?]?\b|[.,;:!?]+)/u';
+    /**
+     * Regex to match each word in paragraph.
+     * 
+     * - Match HTML tag.
+     *  OR 
+     * - Match all following characters until whitespace.
+     */
+    private static $WORD_REGEX = '/(<[^>]+>)|([^\s]+)/um';
 
     public static function init(): void {
         $self = new self();
