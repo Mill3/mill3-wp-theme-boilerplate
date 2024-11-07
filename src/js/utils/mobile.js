ad0fddfba4bd8d8474bc486d9df6b804bbc9ad1b
Dominic Mercier <d.mercier84@gmail.com>
2024-11-07 17:07:19 -0500

    remove last bit of locomotive-scroll code from our boilerplate

--- src/js/utils/mobile.js
+++ src/js/utils/mobile.js
@@ -1,4 +1,3 @@
-// copied from locomotive-scroll to detect if device is mobile
 export const mobile =
   /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ||
   (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
