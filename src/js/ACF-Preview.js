2034eaeb5ca5440a358ff4c0724c48416285ec04
Dominic Mercier <d.mercier84@gmail.com>
2025-05-01 11:52:27 -0400

    ACF-Preview :: dependency free code

--- src/js/ACF-Preview.js
+++ src/js/ACF-Preview.js
@@ -1,48 +1,126 @@
 /* eslint-disable object-shorthand */
 /* eslint-disable no-undef */
 
-// ViteJS glob import all Modules and UIs
+// ViteJS glob import all Modules
 const module_chunks = import.meta.glob('./modules/**/index.js');
-const ui_chunks = import.meta.glob('./ui/**/index.js');
 
 
 import domready from "domready";
-
-import windmill from "@core/windmill";
-// import WindmillFluidTypography from "@core/windmill.fluid-typography";
-import WindmillChunks from "@core/windmill.chunks";
+import EventEmitter2 from "eventemitter2";
 
 import ACF from '@utils/acf';
-import { body } from '@utils/dom';
-import { limit } from '@utils/math';
-import { once } from '@utils/listener';
-import ResizeOrientation from '@utils/resize';
 
 domready(() => {
   const parent = window.parent;
+  const EMITTER = new EventEmitter2({ wildcard: true });
+  const chunks = new Map();
+  const modules = [];
+  const mobile = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
+  const isFunction = (v) => toString.call(v) === "[object Function]" || typeof v === "function";
+
+  class ChunkData {
+    constructor(el, chunk) {
+      this.el = el;
+      this.chunk = chunk;
+    }
+  }
+
+
   const resize = () => {
+    console.log('resize');
+
     const max = Math.ceil(parent.innerHeight * 2 >> 0);
-    window.frameElement.height = limit(25, max, body.scrollHeight);
+    window.frameElement.height = Math.max(25, Math.min(max, document.body.scrollHeight));
+  };
+
+  const importChunks = () => {
+    console.log('import chunks');
+
+    const promises = [];
+    const container = document.body;
+
+    [ ...container.querySelectorAll("[data-module]"), container ].forEach(el => {
+
+      // get data and module or ui chunk type
+      // element should be : <div data-module="my-module"> or <div data-ui="my-ui-js-thing">
+      const { module, moduleNative, moduleBackend } = el.dataset;
+
+      // element can cast 1 or multiple chunk, each seperated by a coma
+      if (module) {
+        const moduleSelector = ACF.is_preview && el.hasAttribute('data-module-backend') ? moduleBackend : (mobile && el.hasAttribute('data-module-native') ? moduleNative : module);
+
+        if( moduleSelector ) {
+          moduleSelector.split(",").forEach(m => {
+            const name = `modules/${m}`;
+
+            // if this chunk as never been imported before, import it
+            if( !chunks.has(name) ) promises.push( importChunk(name) );
+
+            // add element to modules
+            modules.push(new ChunkData(el, name));
+          });
+        }
+      }
+    });
+
+    return Promise.all(promises);
+  };
+  const importChunk = (name) => {
+    console.log('chunk', name);
+    const moduleName = `./${name}/index.js`; // must append index.js to name for matching ViteJS reference
+    let promise = null;
+
+    if( module_chunks[moduleName] ) promise = module_chunks[moduleName]();
+    if( !promise ) return Promise.reject(`Error loading webpack chunk ${name}`);
+
+    // save promise in chunks map
+    chunks.set(name, promise);
+
+    // when loading is completed, update chunks set with default export
+    promise.then(chunk => { chunks.set(name, chunk.default) });
+    promise.catch(error => { console.error(`Error loading chunk ${name}:`, error); });
+
+    return promise;
+  };
+  const createModuleInstances = () => {
+    modules.forEach((module, index) => {
+      if( !(module instanceof ChunkData) ) return;
+
+      const klass = chunks.get(module.chunk);
+      modules[index] = { el: module.el, instance: new klass(module.el, EMITTER) };
+    });
+  };
+  const initModules = () => {
+    modules.forEach(({ instance }) => { if( isFunction(instance.init) ) instance.init(); });
+  };
+  const startModules = () => {
+    modules.forEach(({ instance }) => { if( isFunction(instance.start) ) instance.start(); });
   };
 
   resize();
 
   ACF.is_preview = true;
 
-  ResizeOrientation.add(resize);
-  once(window, 'load', resize);
-  parent.addEventListener('resize', resize, false);
-  windmill.on('done', resize);
+  window.addEventListener('load', resize, { once: true });
+  window.addEventListener('resize', resize);
+  parent.addEventListener('resize', resize, { capture: false });
 
-  //windmill.use( new WindmillFluidTypography() );
-  windmill.use( new WindmillChunks(null, {...module_chunks, ...ui_chunks}) );
-  windmill.init();
 
   // import main styles in dev mode only then resize
   if (process.env.NODE_ENV === "development") {
-    import("../scss/ACF-preview.scss").then(() => {
-      resize();
-    });
+    import("../scss/ACF-preview.scss")
+      .then(resize)
+      .then(importChunks)
+      .then(createModuleInstances)
+      .then(initModules)
+      .then(startModules)
+      .then(resize);
+  } else {
+    importChunks()
+      .then(createModuleInstances)
+      .then(initModules)
+      .then(startModules)
+      .then(resize);
   }
 
 });
